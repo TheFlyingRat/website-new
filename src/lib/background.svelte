ea6646e4c2a8923d13f60bf4570756321faf8ec9
105928712 <105928712@student.swin.edu.au>
2025-06-14 11:27:01 +1000

    change background

--- src/lib/background.svelte
+++ src/lib/background.svelte
@@ -2,37 +2,15 @@
   import { onMount } from 'svelte';
   import { initializeBackground } from '../assets/js/background.js'; 
 
-  let asciiArt = '';
   let background;
-  let selector;
 
-  // Load the ASCII art from the world.txt file
-  onMount(async () => {
-    try {
-      const response = await fetch("/world.txt");
-      if (!response.ok) {
-        throw new Error('Failed to fetch ASCII art.');
-      }
-      asciiArt = await response.text();
-
-      // Call the external script to initialize the background animation
-      initializeBackground(asciiArt, background, selector);
-
-    } catch (error) {
-      console.error('Error loading ASCII art:', error);
-    }
+  onMount(() => {
+    initializeBackground(background);
   });
 </script>
 
 <div class="background-wrapper">
-  <div class="selector" bind:this={selector}>
-    <img alt="Crosshair" width="20px" src="/crosshair.png">
-  </div>
-  <div bind:this={background}>
-    {#if asciiArt}
-      {asciiArt} 
-    {/if}
-  </div>
+  <div bind:this={background}></div>
 </div>
 
 <style lang="scss">
